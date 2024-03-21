d79abc09e307efc92921dd9a74d2c19346ad15f0
debian <andrew_low@ca.ibm.com>
2024-03-21 10:43:44 -0700

    run/pause

--- README.md
+++ README.md
@@ -5,3 +5,14 @@ A couch to 5k running program web app hosted on github pages
 ```
 tidy  -mi -w 120 index.html 
 ```
+
+
+# Development
+
+Using busybox as a local webserver.
+
+```
+docker run -d --rm -p 4000:4000 -v /path/to/src/c25k:/web busybox httpd -h /web -f -v -p 4000
+```
+
+Then visit: [http://localhost:4000](http://localhost:4000)
--- logic.js
+++ logic.js
@@ -45,6 +45,24 @@ workouts = [
 // Action fired when the workout slider is moved
 function changeWorkout() {
 	value = document.getElementById("workout-range").value.toString();
-	document.getElementById("workout").innerHTML = value
+	document.getElementById("workout").innerHTML = 'Workout ' + value
 	
 }
+// button pressed
+function buttonClick() {
+// figure state out by what is in the text
+value = document.getElementById("button").innerHTML
+console.log(value.indexOf("Run"))
+if(value.indexOf("Run") == -1) {
+
+document.getElementById("button").innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-play-fill" viewbox="0 0 16 16" id="button-svg"> <path d= "m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"> </path></svg> Run'
+
+
+} else {
+ document.getElementById("button").innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-pause-btn-fill" viewBox="0 0 16 16"> <path d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5zm5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5z"/> </svg> Pause'
+
+}
+}
+
+// Fire the change workout to update the value
+changeWorkout()
